d3205e0d171921ad0e18083bc4e220a716af3848
Ming-Yen Chung <mingyen@cs.nctu.edu.tw>
2015-11-19 22:50:34 +0800

    add 2048

---
+++ 2048.sh
@@ -0,0 +1,193 @@
+#!/bin/sh
+getc ()
+{
+    stty raw
+    tmp=`dd bs=1 count=1 2>/dev/null`
+    eval $1='$tmp'
+    stty cooked
+}
+
+randpiece()
+{
+    rand=`jot -r 1 1 4`
+    rand2=`jot -r 1 1 4`
+    rand3=`jot -r 1 2 4`
+    while [ $rand3 = 3 ]
+        do
+        rand3=`jot -r 1 2 4`
+        done
+    if [ $((line$rand$rand2)) = 0 ];then
+        eval line$rand$rand2=$rand3
+    else
+        randpiece
+    fi
+}
+
+moveup()
+{
+    for row in $(seq 1 4)
+        do
+        for col in $(seq 2 4)
+            do
+                for compar in $(seq 1 $(($col-1)))
+                    do
+                    if [ $((line$compar$row)) = 0 ];then
+                        eval line$compar$row=$((line$col$row))
+                        eval line$col$row=""
+                   elif [ $((line$compar$row)) = $((line$col$row)) -a $((line$col$row)) != 0 ];then
+                        eval line$compar$row=$(($((line$compar$row))*2))
+                        eval line$col$row=""
+                    fi
+                    done
+            done
+        done
+}
+
+movedown()
+{
+    for row in $(seq 1 4)
+        do
+        for col in $(seq 3 1)
+            do
+                for compar in $(seq 4 $(($col+1)))
+                    do
+                    if [ $((line$compar$row)) = 0 ];then
+                        eval line$compar$row=$((line$col$row))
+                        eval line$col$row=""
+                   elif [ $((line$compar$row)) = $((line$col$row)) -a $((line$col$row)) != 0 ];then
+                        eval line$compar$row=$(($((line$compar$row))*2))
+                        eval line$col$row=""
+                    fi
+                    done
+            done
+        done
+}
+
+moveleft()
+{
+    for col in $(seq 1 4)
+        do
+        for row in $(seq 2 4)
+            do
+                for compar in $(seq 1 $(($row-1)))
+                    do
+                    if [ $((line$col$compar)) = 0 ];then
+                        eval line$col$compar=$((line$col$row))
+                        eval line$col$row=""
+                   elif [ $((line$col$compar)) = $((line$col$row)) -a $((line$col$row)) != 0 ];then
+                        eval line$col$compar=$(($((line$col$compar))*2))
+                        eval line$col$row=""
+                    fi
+                    done
+            done
+        done
+}
+
+moveright()
+{
+    for col in $(seq 1 4)
+        do
+        for row in $(seq 3 1)
+            do
+                for compar in $(seq 4 $(($row+1)))
+                    do
+                    if [ $((line$col$compar)) = 0 ];then
+                        eval line$col$compar=$((line$col$row))
+                        eval line$col$row=""
+                   elif [ $((line$col$compar)) = $((line$col$row)) -a $((line$col$row)) != 0 ];then
+                        eval line$col$compar=$(($((line$col$compar))*2))
+                        eval line$col$row=""
+                    fi
+                    done
+            done
+        done
+}
+
+game()
+{
+
+        winscore=128
+        div="\t---------------------------------\n"
+        div2="\t|\t|\t|\t|\t|\n"
+        while [ true ] 
+            do
+                case $press in
+                    w)
+                        moveup
+                        randpiece
+                        ;;
+                    s)
+                        movedown
+                        randpiece
+                        ;;
+                    a)
+                        moveleft
+                        randpiece
+                        ;;
+                    d)
+                        moveright
+                        randpiece
+                        ;;
+                    q)
+                        for i in $(seq 1 4)
+                                do
+                                    for j in $(seq 1 4)
+                                        do
+                                            echo "line$i$j=$((line$i$j))" >> ./newgame1
+                                        done
+                                done
+                        menu
+                        ;;
+                esac
+                #for i in $(seq 1 4)
+                #    do
+                #        for j in $(seq 1 4)
+                #            do
+                #                if [ $((line$i$j)) = 0 ];then
+                #                    temp=$temp`echo -n "   "`
+                #                else
+                #                    temp=$temp`echo -n "$((line$i$j))"`
+                #                    temp=$temp`echo -e "\t|"`
+                #                fi
+                #            done
+                #        temp=$temp`echo ""`
+                #    done
+                
+                echo -e "\n\n$div$div2\t|$line11\t|$line12\t|$line13\t|$line14\t|\n$div2$div" \
+                "$div2\t|$line21\t|$line22\t|$line23\t|$line24\t|\n$div2$div" \
+                "$div2\t|$line31\t|$line32\t|$line33\t|$line34\t|\n$div2$div" \
+                "$div2\t|$line41\t|$line42\t|$line43\t|$line44\t|\n$div2$div" | dialog --progressbox 25 50
+                getc press
+            done 
+}
+
+menu()
+{
+dialog --title 'menu' --menu "Command Line 2048" 15 50 100 N "New Game" R "Resume" L "Load" S "Save" Q "Quit" 2> /tmp/chotemp
+return=$(cat /tmp/chotemp)
+case $return in
+    N)
+        touch tempgame
+        line11="" line12="" line13="" line14=2
+        line21="" line22=2 line23="" line24=""
+        line31="" line32="" line33="" line34=""
+        line41="" line42="" line43="" line44=""
+        game
+        break
+        ;;
+    R)
+        
+        ;;
+    L) 
+        ;;
+    S) 
+        ;;
+    Q) 
+        echo "Bye"
+        break
+        ;;
+esac
+}
+
+dialog --exit-label "Go" --textbox ./welcome 17 37 && \
+menu
